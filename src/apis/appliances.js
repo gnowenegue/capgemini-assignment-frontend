fb21ce360531bd767473bfa7acf00ed6c1ff58df
Eugene Wong <gnow.enegue@gmail.com>
2020-11-22 22:54:45 +0800

    Load API base url from config.

--- src/apis/appliances.js
+++ src/apis/appliances.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '//localhost:3333/api/',
+  baseURL: process.env.REACT_APP_API_BASEURI,
 });
 
 const getAppliances = async (page, rowsPerPage, filters) => {
